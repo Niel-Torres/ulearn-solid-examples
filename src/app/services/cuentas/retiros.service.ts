eb6e30f214207c010e2cc1829b650949e0456e84
Niel <ntorrema@emeal.nttdata.com>
2022-10-27 21:10:52 +0200

    3.1 L (lSP) NO CUMPLE Principio de Sustitución de Liksov

--- src/app/services/cuentas/retiros.service.ts
+++ src/app/services/cuentas/retiros.service.ts
@@ -1,8 +1,5 @@
 import { CuentaBancaria } from './../../interfaces/cuentas/cuenta-bancaria';
 import { Injectable } from '@angular/core';
-import { CuentaBancariaBasicaComponent } from './../../components/cuentas/cuentaBancariaBasica/cuenta-bancaria-basica/cuenta-bancaria-basica.component';
-import { CuentaBancariaPremiumComponent } from './../../components/cuentas/cuentaBancariaPremium/cuenta-bancaria-premium/cuenta-bancaria-premium.component';
-
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +7,17 @@ import { CuentaBancariaPremiumComponent } from './../../components/cuentas/cuent
 export class RetirosService {
 
   private MONTO_GASTO_ADMIN = 25.00
-   
 
-  constructor(
-    private cuentaBancariaBasica: CuentaBancariaBasicaComponent,
-    private cuentaBancariaPremium: CuentaBancariaPremiumComponent
-  ) { }
+  constructor() { }
 
   // Cargar gastos anuales
-  cargarGastosAdministrativos(cuentas: []) {
-    // Cargar gastos administrativos a todas las cuentas básicas y premium.
-    this.cuentaBancariaBasica.retirar(this.MONTO_GASTO_ADMIN);
-    this.cuentaBancariaPremium.retirar(this.MONTO_GASTO_ADMIN);
+  cargarGastosAdministrativos(cuentas: Array<CuentaBancaria>) {
+    cuentas.forEach(cuenta => {
+      // Cargar gastos administrativos.
+      cuenta.retirar(this.MONTO_GASTO_ADMIN);
+    });
+    
+    
 
   }
 }
